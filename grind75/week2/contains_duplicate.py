d18da79b97af1d50e62ec96c4dfb6f2a37d0154f
MichaelHeinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2024-12-06 09:19:10 +0100

    Add solution for problem Contains Duplicate

---
+++ grind75/week2/contains_duplicate.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return False
+
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+
+        return False
