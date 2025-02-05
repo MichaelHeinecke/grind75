d8537de5340f0fd6e8f6a18738aa9dfcbbfd54ad
MichaelHeinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2025-02-05 14:45:11 +0100

    Add tests for problem reverse bits

---
+++ tests/week3/reverse_bits_test.py
@@ -0,0 +1,15 @@
+import pytest
+
+from grind75.week3.reverse_bits import Solution
+
+
+@pytest.mark.parametrize(
+    "n, expected_result",
+    [
+        (0b00000010100101000001111010011100, 964176192),
+        (0b11111111111111111111111111111101, 3221225471),
+    ],
+)
+def test_bits_are_reversed(n, expected_result):
+    actual_result = Solution().reverseBits(n)
+    assert actual_result == expected_result
