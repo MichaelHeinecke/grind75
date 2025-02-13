174341c971d025057fae03a32f036ed7af6c7d50
MichaelHeinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2025-02-13 09:46:09 +0100

    Add test for problem implement trie

---
+++ tests/week5/implement_trie_test.py
@@ -0,0 +1,11 @@
+from grind75.week5.implement_trie import Trie
+
+
+def test_trie_works_correctly():
+    trie = Trie()
+    trie.insert("apple")
+    assert trie.search("apple") is True
+    assert trie.search("app") is False
+    assert trie.startsWith("app") is True
+    trie.insert("app")
+    assert trie.search("app") is True
