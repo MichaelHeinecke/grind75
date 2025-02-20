35309e64df326c47f39dfdc675094ac774ae2b7c
MichaelHeinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2025-02-20 22:40:39 +0100

    Add solution for problem min stack

---
+++ grind75/week5/min_stack.py
@@ -0,0 +1,18 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minimum = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.minimum.append(min(self.minimum[-1], val) if self.minimum else val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minimum.pop()
+
+    def top(self) -> int:
+        return self.stack[-1] if self.stack else None
+
+    def getMin(self) -> int:
+        return self.minimum[-1] if self.minimum else None
